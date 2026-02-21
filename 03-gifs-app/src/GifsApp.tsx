2f3209b11b47cfdd9027f347aae0b830e69d876e
Cristhian Camacho <cristhian.camacho@gulfstream.com>
2026-02-20 16:56:27 -0800

    gifs 03 finished

--- 03-gifs-app/src/GifsApp.tsx
+++ 03-gifs-app/src/GifsApp.tsx
@@ -2,24 +2,29 @@ import { CustomHeader } from "./shared/components/CustomHeader";
 import { PreviousSearches } from "./gifs/components/PreviousSearches";
 import { SearchBar } from "./shared/components/SearchBar";
 import { GifList } from "./gifs/components/GifList";
-import { mockGifs } from "./mock-data/gifs.mock";
 import { useState } from "react";
+import { getGifsByQuery } from "./gifs/actions/get-gifs-by-query.action";
+import type { Gif } from "./gifs/interfaces/gif.interface";
 
 export const GifsApp = () => {
-  const [previousTerms, setPreviousTerms] = useState(["dragon ball z"]);
+  const [previousTerms, setPreviousTerms] = useState<string[]>([]);
+  const [gifs, setGifs] = useState<Gif[]>([]);
 
   const handleTermClicked = (term: string) => {
     console.log({ term });
   };
-// https://api.giphy.com/v1/gifs/search?api_key=ib5Vp0x6JjX0GSrxxlxKH4E4x6Pc8KzA&q=saitama&limit=10&lang=en
-// ib5Vp0x6JjX0GSrxxlxKH4E4x6Pc8KzA api key
-  const handleSearch = (query: string = "") => {
+
+  const handleSearch = async (query: string = "") => {
     query = query.trim().toLowerCase();
     if (query.length === 0) return;
 
     if (previousTerms.includes(query)) return;
 
     setPreviousTerms([query, ...previousTerms.splice(0, 7)]);
+
+    const gifs = await getGifsByQuery(query);
+
+    setGifs(gifs);
   };
   return (
     <>
@@ -40,7 +45,7 @@ export const GifsApp = () => {
       />
 
       {/* gifs */}
-      <GifList gifs={mockGifs} />
+      <GifList gifs={gifs} />
     </>
   );
 };
