3cb10fa3cf3e6f2cdad734cefdd6fdeb3c910344
Cristhian Camacho <cristhian.camacho@gulfstream.com>
2026-02-26 16:59:15 -0800

    03 cusrtomhook

---
+++ 04-hooks-app/src/hooks/useTrafficLight.tsx
@@ -0,0 +1,66 @@
+import { type useState, useEffect } from "react";
+
+const colors = {
+  red: "bg-red-500 animate-pulse",
+  yellow: "bg-yellow-500 animate-pulse",
+  green: "bg-green-500 animate-pulse",
+  blue: "bg-blue-500 animate-pulse",
+};
+
+type TrafficLightColor = "red" | "green" | "yellow" | "blue";
+
+export const useTrafficLight = (color = "red") => {
+  const [light, setLight] = useState<TrafficLightColor>(color);
+
+  const [countdown, setCountdown] = useState(5);
+
+  //Countdown effect
+  useEffect(() => {
+    if (countdown === 0) return;
+
+    const intervalId = setInterval(() => {
+      setCountdown((prev) => prev - 1);
+    }, 1000);
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, [countdown]);
+
+  //Change Light color effect
+  useEffect(() => {
+    if (countdown > 0) return;
+
+    setCountdown(5);
+
+    if (light === "red") {
+      setLight("green");
+      return;
+    }
+
+    if (light === "yellow") {
+      setLight("red");
+      return;
+    }
+
+    if (light === "green") {
+      setLight("yellow");
+      return;
+    }
+
+    return;
+  }, [countdown, light]);
+
+  return {
+    //Props
+    countdown,
+
+    //Computed
+    percentage: (countdown / 5) * 100,
+    greenLight: light === "green" ? colors.green : "bg-gray-500",
+    yellowLight: light === "yellow" ? colors.yellow : "bg-gray-500",
+    redLight: light === "red" ? colors.red : "bg-gray-500",
+
+    //Methods
+  };
+};
--- 04-hooks-app/src/main.tsx
+++ 04-hooks-app/src/main.tsx
@@ -4,12 +4,17 @@ import { createRoot } from 'react-dom/client'
 // import { HooksApp } from './HooksApp'
 
 import './index.css'
-import { TrafficLight } from './01-useState/TrafficLight'
+// import { TrafficLight } from './01-useState/TrafficLight'
+// import { TrafficLightWithEffect } from './02-useEffect/TrafficLightWithEffect'
+// import { TrafficLightWithHook } from './02-useEffect/TrafficLightWithHook'
 
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
    {/* <HooksApp/> */}
-   <TrafficLight/>
+   {/* <TrafficLight/> */}
+   {/* <TrafficLightWithEffect/> */}
+   {/* <TrafficLightWithHook/> */}
+
   </StrictMode>,
 )
