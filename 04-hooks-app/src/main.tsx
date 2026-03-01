ec26f877d18b156a8c9adb4efedd655649f90d83
Cristhian Camacho <cristhian-eduardo82@hotmail.com>
2026-02-28 18:05:51 -0800

    seccion 10 empezada

--- 04-hooks-app/src/main.tsx
+++ 04-hooks-app/src/main.tsx
@@ -1,20 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 
 // import { HooksApp } from './HooksApp'
 
-import './index.css'
+import "./index.css";
+// import { PokemonPage } from "./03-examples/PokemonPage";
+import { FocusScreen } from "./04-useRef/FocusScreen";
 // import { TrafficLight } from './01-useState/TrafficLight'
 // import { TrafficLightWithEffect } from './02-useEffect/TrafficLightWithEffect'
 // import { TrafficLightWithHook } from './02-useEffect/TrafficLightWithHook'
 
-
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-   {/* <HooksApp/> */}
-   {/* <TrafficLight/> */}
-   {/* <TrafficLightWithEffect/> */}
-   {/* <TrafficLightWithHook/> */}
-
+    {/* <HooksApp/> */}
+    {/* <TrafficLight/> */}
+    {/* <TrafficLightWithEffect/> */}
+    {/* <TrafficLightWithHook/> */}
+    {/* <PokemonPage /> */}
+    <FocusScreen />
   </StrictMode>,
-)
+);
