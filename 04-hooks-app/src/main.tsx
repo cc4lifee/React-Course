1340c4920703755ec921a255ce14ddc0c7c96f06
Cristhian Camacho <cristhian-eduardo82@hotmail.com>
2026-03-02 19:52:03 -0800

    install ui framework

--- 04-hooks-app/src/main.tsx
+++ 04-hooks-app/src/main.tsx
@@ -6,6 +6,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 // import { PokemonPage } from "./03-examples/PokemonPage";
 import { FocusScreen } from "./04-useRef/FocusScreen";
+import { TasksApp } from "./05-useReducer/TaskApp";
 // import { TrafficLight } from './01-useState/TrafficLight'
 // import { TrafficLightWithEffect } from './02-useEffect/TrafficLightWithEffect'
 // import { TrafficLightWithHook } from './02-useEffect/TrafficLightWithHook'
@@ -17,6 +18,7 @@ createRoot(document.getElementById("root")!).render(
     {/* <TrafficLightWithEffect/> */}
     {/* <TrafficLightWithHook/> */}
     {/* <PokemonPage /> */}
-    <FocusScreen />
+    {/* <FocusScreen /> */}
+    <TasksApp />
   </StrictMode>,
 );
